bec1a7e52621a3a01c61ba1e90ff63a663e18ff5
piotrekkruk <piotrekkruk@icloud.com>
2022-09-04 11:40:26 +0200

    1.0.0

--- main.py
+++ main.py
@@ -1,8 +1,57 @@
-# This is a sample Python script.
+#Step 5
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import hangman_words
+import hangman_art
+
+
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+print(hangman_art.logo)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter in display:
+            print("This character is already guessed.")
+        if letter == guess:
+            display[position] = letter
+
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+
+        lives -= 1
+        print(f"{guess} is not in the word")
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
